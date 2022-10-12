4410a55352fa7949a1463336d50742d21777eb35
CrisDeniz <cdgm.7star@gmail.com>
2022-10-12 18:46:31 -0300

    improving responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,8 +50,13 @@ const URL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&lang=pt_b
  
 
   return (
-    
+
     <div>
+
+      <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
+      </Head>
+
       <div className={styles.container} >
       <span>Tempo Agora</span>
       <div className={styles.search}>
